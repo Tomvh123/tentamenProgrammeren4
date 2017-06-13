bbf67b8c89250022394c558a07154796f008bc05
jeffry120 <jeffry-1997@outlook.com>
2017-06-13 17:48:44 +0200

    update-delete routes

--- routes/rental.routes.v1.js
+++ routes/rental.routes.v1.js
@@ -77,4 +77,24 @@ router.get('*', function (req, res) {
 
 });
 
+router.post('/login', function(req, res) {
+
+    console.dir(req.body);
+
+    var username = req.body.username;
+    var password = req.body.password;
+
+
+    if (username == _dummy_username && password == _dummy_password) {
+        var token = auth.encodeToken(username);
+        res.status(200).json({
+            "token": token,
+        });
+    } else {
+        console.log('Input: username = ' + username + ', password = ' + password);
+        res.status(401).json({ "error": "Invalid credentials, bye" })
+    }
+
+});
+
 module.exports = router;
