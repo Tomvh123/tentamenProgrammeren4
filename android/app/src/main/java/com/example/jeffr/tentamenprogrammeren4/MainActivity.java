7af5c0ae43366c38d4a095ea74d529c0a94b08d2
Tomvh123 <thaaster@avans.nl>
2017-06-16 12:36:12 +0200

    bezig met het converten van todo

--- android/app/src/main/java/com/example/jeffr/tentamenprogrammeren4/MainActivity.java
+++ android/app/src/main/java/com/example/jeffr/tentamenprogrammeren4/MainActivity.java
@@ -1,9 +1,29 @@
 package com.example.jeffr.tentamenprogrammeren4;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity  {
+import java.util.ArrayList;
+
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, AdapterView.OnItemClickListener {
+
+    public final String TAG = this.getClass().getSimpleName();
+
+    public final static String MOVIE_DATA = "MOVIE";
+
+    public static final int MY_REQUEST_CODE = 1234;
+
+    private ListView listView;
+    private BaseAdapter movieAdapter;
+    private ArrayList<Movie> movies = new ArrayList<>();
 
 
     @Override
@@ -11,8 +31,25 @@ public class MainActivity extends AppCompatActivity  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if(tokenAvailable()){
+            setContentView(R.layout.activity_main);
+
+            listView = (ListView) findViewById(R.id.listview);
+            listView.setOnItemClickListener(this);
+            
+        }
+
 
 
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+    }
 }
---
+++ android/app/src/main/java/com/example/jeffr/tentamenprogrammeren4/domain/MovieAdapter.java
@@ -0,0 +1,8 @@
+package com.example.jeffr.tentamenprogrammeren4.domain;
+
+/**
+ * Created by tom on 16-6-2017.
+ */
+
+public class MovieAdapter {
+}
