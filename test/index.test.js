44d449651b1a87d059e89df4b91e3d3e22d35122
jeffry120 <jeffry-1997@outlook.com>
2017-06-19 19:34:21 +0200

    test

--- test/index.test.js
+++ test/index.test.js
@@ -2,15 +2,15 @@
 
 var chai = require('chai');
 var chaiHttp = require('chai-http');
-var server = require('../server');
+var server = require('/index');
 var chould = chai.should();
 
 chai.use(chaiHttp);
 
-describe('Hello', function(){
-	it('GET /api/v1/hello', function(done){
+describe('Working', function(){
+	it('POST /api/v1/login', function(done){
 		chai.request(server)
-			.get('/api/v1/hello')
+			.post('/api/v1/login')
 			.end( function(err, res){
 				res.should.have.status(200);
 				res.body.should.be.a('object');
